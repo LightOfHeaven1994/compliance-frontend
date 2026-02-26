ea8ef3d12c8d3a65ecee6f032d28bc76b2736865
Egor Shamardin <33912805+LightOfHeaven1994@users.noreply.github.com>
2026-02-26 14:54:58 +0100

    fix(RHINENG-23930): Add kessel context for Inventory Compliance (#2748)

--- src/Modules/ComplianceDetails.js
+++ src/Modules/ComplianceDetails.js
@@ -4,19 +4,34 @@ import { init } from 'Store';
 import Details from '../SmartComponents/SystemDetails/Details';
 import { RBACProvider } from '@redhat-cloud-services/frontend-components/RBACProvider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AccessCheck } from '@project-kessel/react-kessel-access-check';
+import useFeatureFlag from 'Utilities/hooks/useFeatureFlag';
+import { KESSEL_API_BASE_URL } from '@/constants';
 
 const queryClient = new QueryClient();
 
 const ComplianceDetails = (props) => {
   const store = useRef(init().getStore());
+  const isKesselEnabled = useFeatureFlag('compliance.kessel_enabled');
 
   return (
     <QueryClientProvider client={queryClient}>
-      <RBACProvider appName="compliance">
-        <Provider store={store.current}>
-          <Details {...props} />
-        </Provider>
-      </RBACProvider>
+      {isKesselEnabled ? (
+        <AccessCheck.Provider
+          baseUrl={window.location.origin}
+          apiPath={KESSEL_API_BASE_URL}
+        >
+          <Provider store={store.current}>
+            <Details {...props} />
+          </Provider>
+        </AccessCheck.Provider>
+      ) : (
+        <RBACProvider appName="compliance">
+          <Provider store={store.current}>
+            <Details {...props} />
+          </Provider>
+        </RBACProvider>
+      )}
     </QueryClientProvider>
   );
 };
